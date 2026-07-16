d4d53748cfb247487ce452810a8db73652020ed9
SD- Software Development <softdev@SD-s-iMac.local>
2026-07-16 11:00:11 +0530

    Configured debug env to make live api call

--- env/debug.env.ts
+++ env/debug.env.ts
@@ -11,6 +11,7 @@ const debugEnvironment: any = {
     // APP_HOST_AI_DOMAIN: 'http://localhost:20156', // Python AI app port
     // APP_LISTEN_HOST: 'localhost',
 
+    
     APP_HOST_GRAPHQL_DOMAIN: 'http://0.0.0.0:20147',
     APP_HOST_WEBSOCKET_DOMAIN: 'ws://0.0.0.0:20150',
     APP_HOST_REST_DOMAIN: 'http://0.0.0.0:20152',
@@ -18,8 +19,8 @@ const debugEnvironment: any = {
     APP_HOST_AI_DOMAIN: 'http://0.0.0.0:20156',
     APP_LISTEN_HOST: '0.0.0.0',
 
-    BFW_API_SDK_JWT_ACCESS_TOKEN: 'yJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImF2MSJ9.eyJzdWIiOiJNUSIsInVzZXJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImluZm9AdGhhdHNlbmQuY29tIiwicm9sZV9pZCI6MSwidHlwZSI6ImFjY2Vzc190b2tlbiIsImlhdCI6MTc4MzQ4NTQwNiwiZXhwIjoxNzgzNTcxODA2LCJhdWQiOiJBcHBsaWNhdGlvbiIsImlzcyI6IlRIQVRTRU5EIn0.TfrkEN1xADiiq7YXTEoZvc0SMAOlXpjPGIxAjwLwCA8Fz460crPmGsgBaZHv8HDR6BgyLmAh9i5MqLEftQGDld2soaCMQBCPVT2qZgB_Q_TvKpLCWED7_eE5qGFH522JlIEHi1914_dAXxiWCbEV0tArvJP5OfogJxQ1vIKJ1pgwLAFOVZCz7If5ZFGBTTgYD9cBtQMVaDVGRTpr6Z1pOTzB2isntvU2C4bEu9_HaqB0Pg8xuMLlG13J2KhbEkFMsgJ0TNfCtURQg4dSrWYkz3qIiUAz14bopRbuHfgOncBu8x9X6PgmC2T6IOV9MoJ0WACi52h-dPVB0eeNPpa94g',
-    BFW_API_SDK_JWT_REFRESH_TOKEN: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InJ2MSJ9.eyJzdWIiOiJNUSIsInVzZXJuYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImluZm9AdGhhdHNlbmQuY29tIiwicm9sZV9pZCI6MSwidHlwZSI6InJlZnJlc2hfdG9rZW4iLCJpYXQiOjE3ODM0ODU0MDYsImV4cCI6MTc4NDA5MDIwNiwiYXVkIjoiQXBwbGljYXRpb24iLCJpc3MiOiJUSEFUU0VORCJ9.i6BEx-_6vZHdLryEc9AJYj3xFWfXsPQSxYG-VtnGDNU2V1JiA0RMH1UlTtbOPqiqAcN-vohgcUGmNzrmxmade9HmnmhDcpxdGnh9J7e27XIPUfZchmrmp_DiBove75LwumPMcr4zot5xhdvRaBhetbKGV9a1a7ombhicDtl4xmEkBRxJ-QwxK1Fzv0g2kXevYZac2wCHAg5AhS9WLSCqov0FZcSZGkdN31izRKfLkGMNXyG9OSc2oDbbmAFUO66g30oD6PRO_3SyYglQsZpTuvJBy03uLW0AwQDwaZKmHgVsrXeGD7HZe-E4VEdenX3gVF_E3MrJfv1LiwD2bNtA9Q',
+    BFW_API_SDK_JWT_ACCESS_TOKEN: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImF2MSJ9.eyJzdWIiOiJNUSIsInVzZXJuYW1lIjoibWFoamZpdCIsImVtYWlsIjoibWFoamZpdGdhbWVAZ21haWwuY29tIiwicm9sZV9pZCI6MCwidHlwZSI6ImFjY2Vzc190b2tlbiIsImlhdCI6MTc4MzUwMjI4MywiZXhwIjoxNzgzNTg4NjgzLCJhdWQiOiJBcHBsaWNhdGlvbiIsImlzcyI6IlRIQVRTRU5EIn0.TAxNa7udlYJVMe-CFfBOReYYCbAHql4aXxkpbDAg46qMlWHP-NN_XTMpaADZ_7CIkZSO76V_6GnQOtm_Z-sHuLNcRtQxb-DCqxeCZ8S8QgIL5aJMAx1I3hzeUC8kI7fGwx_TPqM3HzPp7xhR8yVIXPgQ-BQ6DdQJCaCtuXAg4NIyX0q5SmIaYfvr1UZqwdq2gg4sjNrtYKAJ1hkAQ9Umlkwr9Y0B3syCh5PvHJK6Rshq42Lthg2s13cQlgl7XQ2KruAdeWX6I7Y5HENEcyqH-BONXAfYwRzjvRkGEBe9ibzG1jqG-1ZsHWKZKDUFM-x5uUOJLkGYoHSWZOUKTjatdw',
+    BFW_API_SDK_JWT_REFRESH_TOKEN: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6InJ2MSJ9.eyJzdWIiOiJNUSIsInVzZXJuYW1lIjoibWFoamZpdCIsImVtYWlsIjoibWFoamZpdGdhbWVAZ21haWwuY29tIiwicm9sZV9pZCI6MCwidHlwZSI6InJlZnJlc2hfdG9rZW4iLCJpYXQiOjE3ODM1MDIyODMsImV4cCI6MTc4NDEwNzA4MywiYXVkIjoiQXBwbGljYXRpb24iLCJpc3MiOiJUSEFUU0VORCJ9.DRFOYeb6huxNZYyrYSx4N3Iz8JTkCJSIyttxbmUwumFPof6Zhj4xztcvg3CrSdyv95B58qBKSfvcCahFECdK1ni3MxL0Cw3RN15lhKlpDCrA4mI4nwrz9AsfkVLWqKNI7PcdZ04_53Gtdrk_qhjgLajKvf2Zl8lFaWsHbCn7EVhOpFRDSqt1Bp6Mlm0YUtXi9xO4MsS8AUZAo8FaQ8NbWSbS1d98jaGEO5Dw3hpIYSaeDDXpx5YKNw8aFAaM5M09k-66d_Iz_bxpaocHr-AGS0xE441516v9TBTct64zN3Ysi9q-iVKoSfDRcxj2d0b0cZD-jEISuKmNpncbwx54GA',
     
     //BFW_API_SDK_GRAPHQL_URL: 'http://localhost:20147/graphql',
     //BFW_API_SDK_REST_URL: 'http://localhost:20152/rest',
@@ -33,8 +34,8 @@ const debugEnvironment: any = {
     BFW_API_SDK_REST_URL: 'https://192.168.0.230:20152/rest',
     BFW_API_SDK_WS_URL: 'https://192.168.0.230:20150', */
 
-    BFW_API_SDK_GRAPHQL_URL: 'https://bfw-nestjs-microservice-api.thatsend.app/graphql',
-    BFW_API_SDK_REST_URL: 'https://bfw-nestjs-microservice-api.thatsend.app/rest',
-    BFW_API_SDK_WS_URL: 'https://bfw-nestjs-microservice-ws.thatsend.app',
+    BFW_API_SDK_GRAPHQL_URL: 'https://api-mahjfit-com.thatsend.dev/graphql',
+    BFW_API_SDK_REST_URL: 'https://api-mahjfit-com.thatsend.dev/rest',
+    BFW_API_SDK_WS_URL: 'https:/https://api-mahjfit-com.thatsend.dev',
 };
 export const environment: any = {...defaultEnvironment, ...debugEnvironment};
